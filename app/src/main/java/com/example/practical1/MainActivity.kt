bd1fb988239fcc292e2d12814e9f9ed463bed97c
jiangyifan2004 <3113793438@qq.com>
2026-09-24 17:23:52 +0800

    Create LinearLayout responsive interface

---
+++ app/src/main/java/com/example/practical1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.practical1
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.statusBarColor = Color.BLACK
+        window.navigationBarColor = Color.rgb(255, 248, 255)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+        setContentView(R.layout.activity_main)
+    }
+}
